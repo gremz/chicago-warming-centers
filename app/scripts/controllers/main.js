494e7c1b3236828980053bc4e308ea35369b0c4f
remremz <rembitsystems@gmail.com>
2013-11-05 11:08:32 -0600

    refactored get mapData into factory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,21 @@
 'use strict';
 
+// Singleton factory
+angular.module('mapApp').factory('MapData', function($http) {
+  var dataUrls = ['http://data.cityofchicago.org/resource/h243-v2q5.json?$jsonp=JSON_CALLBACK', 'warm_data.json'],
+      resourceIndex = 0,
+      dataUrl = dataUrls[resourceIndex];
+  return {
+    get: function(callback) {
+      var requestType = (dataUrls[resourceIndex].indexOf('jsonp') > -1) && 'jsonp' || 'get';
+
+      return dataUrls[resourceIndex] ? $http[requestType](dataUrls[resourceIndex++]) : { status: 404 };
+    }
+  }
+});
+
 angular.module('mapApp')
 .controller('MainCtrl', function ($scope, $cookies) {
-  $scope.awesomeThings = [
-  'HTML5 Boilerplate',
-  'AngularJS',
-  'Karma'
-  ];
-  $scope.data = { message: 'FLAG!'};
-
   if (!$cookies.firstTimer) {
     $cookies.firstTimer = '1';
     $('#infoModal').modal('show');
@@ -16,107 +23,89 @@ angular.module('mapApp')
 });
 
 angular.module('mapApp')
-.controller('GetData', function($scope, $http) {
+.controller('GetData', function($scope, MapData) {
   //data references
-  var dataUrls = ['http://data.cityofchicago.org/resource/h243-v2q5.json?$jsonp=JSON_CALLBACK', 'warm_data.json'],
-      $alert = $('.alert');
+  var $alert = $('.alert');
 
-  fetchMapData(dataUrls[0], 0);
+  // Define data scope
+  !$scope.data && ($scope.data = {});
 
-  // fetchMapData
-  function fetchMapData(dataUrl, resourceIndex) {
-    var requestType = (dataUrl.indexOf('jsonp') > -1) && 'jsonp' || 'get';
+  function handleRequest(response) {
+      if (response.data) {
+        addDataPoints(response.data);
+      } else if (response.status == 404) {
+        // handle total failure
+      } else {
+        MapData.get().then(handleRequest, handleRequest);
+      }
+  }
 
-    $http[requestType](dataUrl).
-      // on success return data and status
-      success(function(data, status, headers, config) {
-        window.head = resourceIndex;
-        $scope.data.status = !resourceIndex && 'Data OK!' || ('Data backup #' + resourceIndex + ' used'); // if index >0 return error msg
-        $scope.data.rawData = data;
+  function flashAlert($el) {
+    var alertTopHeight = $(window).height()-38;
+    $el.css({'position': 'absolute', 'top': alertTopHeight}).show().fadeOut(2500);
+  }
 
-        // Reset data
-        $scope.data.resetData = angular.copy($scope.data.rawData);
+  function addDataPoints(rawData) {
+    var location,
+        marker;
+    for (var i=0; i < rawData.length; i++) {
+      location = rawData[i].location;
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location.latitude, location.longitude),
+        map: wcm.map,
+        title: rawData[i].site_name,
+        uId: rawData[i].location.longitude.split('.')[1]
+      });
 
-        addDataPoints(data);
-
-        flashAlert($alert);
-      }).
-      // If failed go to next resource or fail with message
-      error(function(data, status, headers, config) {
-        var nextResource = dataUrls[resourceIndex+1];
-
-        $scope.data.status = 'Failed to fetch data! Attempting backup...';
-        nextResource && fetchMapData(nextResource, resourceIndex+1);
-        // If hidden show
-        $alert.css('display') == 'none' && flashAlert($alert);
+      // "this" is the current marker
+      google.maps.event.addListener(marker, 'click', function() {
+        wcm.map.setZoom(13);
+        wcm.map.setCenter(this.getPosition());
+        // Add filter
+        var $input = $('.search').first();
+        $input.val(this.title);
+        $input.trigger('input');
+        $('.description[data-uid="' + this.uId + '"').show();
       });
     }
+  }
 
-    function flashAlert($el) {
-      var alertTopHeight = $(window).height()-38;
-      $el.css({'position': 'absolute', 'top': alertTopHeight}).show().fadeOut(2500);
+  // loadMap
+  (function loadMap(rawData) {
+    if (document && document.getElementById('map-canvas')) {
+      var mapOptions = {
+        center: new google.maps.LatLng(41.847, -87.702),
+        zoomControl: true,
+        disableDefaultUI: true,
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      },
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      (window.wcm || (window.wcm = {})).map = map;
+
+      // Add map events
+
+    } else {
+      setTimeout(function() {
+        loadMap();
+      },250);
     }
+  })();
 
-    function addDataPoints(rawData) {
-      var location,
-          marker;
-      for (var i=0; i < rawData.length; i++) {
-        location = rawData[i].location;
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(location.latitude, location.longitude),
-          map: wcm.map,
-          title: rawData[i].site_name,
-          uId: rawData[i].location.longitude.split('.')[1]
-        });
+  // Toggle location description
+  $scope.showInfo = function($event) {
+    $event.preventDefault();
+    var $title = $($event.currentTarget);
+    $title.siblings().toggle();
+    $title.find('.caret').first().toggle();
+    $title.find('.open-caret').toggle();
+  };
 
-        // "this" is the current marker
-        google.maps.event.addListener(marker, 'click', function() {
-          wcm.map.setZoom(13);
-          wcm.map.setCenter(this.getPosition());
-          // Add filter
-          var $input = $('.search').first();
-          $input.val(this.title);
-          $input.trigger('input');
-          $('.description[data-uid="' + this.uId + '"').show();
-        });
-      }
-    }
+  $scope.clearQuery = function() {
+    var $searchInput = $('.search-container .search');
+    $scope.query = '';
+  };
 
-    // loadMap
-    (function loadMap(rawData) {
-      if (document && document.getElementById('map-canvas')) {
-        var mapOptions = {
-          center: new google.maps.LatLng(41.847, -87.702),
-          zoomControl: true,
-          disableDefaultUI: true,
-          zoom: 11,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        (window.wcm || (window.wcm = {})).map = map;
-        console.log('map loaded');
+  handleRequest({});
 
-        // Add map events
-
-      } else {
-        setTimeout(function() {
-          loadMap();
-        },250);
-      }
-    })();
-
-    // Toggle location description
-    $scope.showInfo = function($event) {
-      $event.preventDefault();
-      var $title = $($event.currentTarget);
-      $title.siblings().toggle();
-      $title.find('.caret').first().toggle();
-      $title.find('.open-caret').toggle();
-    };
-
-    $scope.clearQuery = function() {
-      var $searchInput = $('.search-container .search');
-      $scope.query = '';
-    };
-
-  });
+});
